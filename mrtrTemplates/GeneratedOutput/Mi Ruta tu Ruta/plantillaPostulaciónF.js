96a741297ee73df1cfbbca2cde30dac8c6b35307
cdanielm <cdanielm58@gmail.com>
2017-06-01 22:39:27 -0500

    archivos separados

---
+++ mrtrTemplates/GeneratedOutput/Mi Ruta tu Ruta/plantillaPostulaciónF.js
@@ -0,0 +1,16 @@
+
+.controller('PostulaciónF', function($scope, $cordovaSQLite) {
+
+  $scope.listPostulaciónF = function() {
+    var query = "SELECT * FROM PostulaciónF";
+    $cordovaSQLite.execute(db, query);
+  }
+
+  $scope.addPostulaciónF = function(){
+    var query = "INSERT INTO PostulaciónF (FechaPostulacion,EstadoPostulacion,) VALUES (?, ?)";
+    $cordovaSQLite.execute(db, query, [$scope.FechaPostulacion,$scope.EstadoPostulacion,]);
+     $scope.FechaPostulacion='';
+     $scope.EstadoPostulacion='';
+  }
+
+});
